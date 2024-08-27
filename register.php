3dc28c0f740da02c0eb2af8917e6d50ec26edb1b
Maksym Shcherbak <dev.14805@simplelogin.co>
2024-08-27 20:48:40 +0300

    create chat in pdo

--- register.php
+++ register.php
@@ -1,13 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-<?php include("header.php")?>
-    <form action="user.php" method="post">
+    <?php
+    session_start();
+    include("header.php");
+    ?>
+    <form action="register.php" method="post">
         <label for="firstName">First Name</label>
         <br>
         <input type="text" name="firstName" id="firstName">
@@ -30,5 +35,34 @@
         <br>
         <input type="submit">
     </form>
+    <?php
+    // Настройки подключения к базе данных
+    include("pdo.php");
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        // SQL запрос для вставки данных
+        $sql = "INSERT INTO users (firstName, lastName, dateOfBirth, `login`, `password`) VALUES (:firstName, :lastName, :dateOfBirth, :login, :password)";
+
+        // Подготовка SQL запроса
+        $stmt = $pdo->prepare($sql);
+
+        // Данные для вставки
+        $name = $_POST["firstName"];
+        $lname = $_POST["lastName"];
+        $date = $_POST["dateOfBirth"];
+        $login = $_POST["login"];
+        $password = $_POST["password"];
+
+        $_SESSION["login"] = $login;
+        $_SESSION["password"] = $password;
+
+        // Привязка параметров и выполнение запроса
+        var_dump($stmt->execute(['firstName' => $name, 'lastName' => $lname, 'dateOfBirth' => $date, 'login' => $login, 'password' => $password]));
+        $_SESSION["id"] = $pdo->lastInsertId();
+        echo "Данные успешно вставлены!";
+
+        header("Location:/user.php");
+    }
+    ?>
 </body>
+
 </html>
